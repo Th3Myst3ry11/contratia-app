46c9e88986155a6313296c3aa701a49d9a42e843
Th3Myst3ry11 <caetanobeltor@gmail.com>
2023-01-23 02:00:56 +0000

    login google added

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -87,7 +87,7 @@
             <a
               class="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
               style="background-color: #fa2500"
-              href="#!"
+              href="{{route('google-auth')}}"
               role="button"
               data-mdb-ripple="true"
               data-mdb-ripple-color="light"
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VerificationController;
@@ -54,3 +55,9 @@ Route::group(['middleware' => ['auth']], function() {
             Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
     });
 });
+
+//GOOGle Auth 
+Route::get('auth/google', [GoogleAuthController::class, 'redirect'])->name('google-auth');
+Route::get('auth/google/callback', [GoogleAuthController::class, 'callbackGoogle']);
+
+
